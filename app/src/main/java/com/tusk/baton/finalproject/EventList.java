3de1df1a0125fa669761281306ca16a713d3ea33
tomoestreich <tom.oestreich@gmail.com>
2017-04-26 03:02:04 -0400

    Adds 6 cards to layout programmatically.

---
+++ app/src/main/java/com/tusk/baton/finalproject/EventList.java
@@ -0,0 +1,9 @@
+package com.tusk.baton.finalproject;
+
+/**
+ * Created by tomoestreich on 4/26/2017.
+ */
+
+public class EventList {
+
+}
--- app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
+++ app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
@@ -5,17 +5,50 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
 
     private TextView mTextMessage;
+    private TextView TextMessage;
     private CardView eventCard;
+    private ScrollView scrollView;
+    private ViewGroup cardStack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        eventCard = (CardView) findViewById(R.id.card_view);
+//        mTextMessage = (TextView) eventCard.findViewById(R.id.info_text);
+//        cardStack = (ViewGroup) findViewById(R.id.cardStack);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        cardStack = (LinearLayout) findViewById(R.id.cardStack);
+
+        for(int i = 0; i < 6; i++){
+            System.out.println(i);
+            View child = getLayoutInflater().inflate(R.layout.event_layout, null);
+            CardView eventCard;
+            eventCard = (CardView) child.findViewById(R.id.card_view);
+            mTextMessage = (TextView) eventCard.findViewById(R.id.info_text);
+            mTextMessage.setText("Card Number: " + i);
+            cardStack.addView(child);
+        }
+
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,20 +70,5 @@ public class MainActivity extends AppCompatActivity  {
         }
 
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        eventCard = (CardView) findViewById(R.id.card_view);
-        mTextMessage = (TextView) eventCard.findViewById(R.id.info_text);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
-
-    }
 
 }
