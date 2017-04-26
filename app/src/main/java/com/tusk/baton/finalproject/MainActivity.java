2ec29814fa53271be2948aec49d6e42076d4c05e
Sushant Bhattarai <swooshant@gmail.com>
2017-04-26 00:47:44 -0400

    Rough implementation of cardview

--- app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
+++ app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
@@ -1,15 +1,21 @@
 package com.tusk.baton.finalproject;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private TextView mTextMessage;
+    private CardView eventCard;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText("navigation");
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -37,9 +43,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        eventCard = (CardView) findViewById(R.id.card_view);
+        mTextMessage = (TextView) eventCard.findViewById(R.id.info_text);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
     }
 
 }
