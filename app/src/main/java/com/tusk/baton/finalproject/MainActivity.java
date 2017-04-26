843e0ed66fce2f645e223f2b950ec0f1591e5945
Sushant Bhattarai <swooshant@gmail.com>
2017-04-26 03:05:55 -0400

    Action/Toolbar bar done

--- app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
+++ app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
@@ -8,14 +8,20 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
 
+    private static final String TAG = "Menu Buttons~: ";
     private TextView mTextMessage;
     private CardView eventCard;
+    private  Toolbar myToolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +45,22 @@ public class MainActivity extends AppCompatActivity  {
     };
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.user_profile:
+                Log.d(TAG, "User Profile" );
+                System.out.println("Pressed User Profile");
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -46,11 +68,28 @@ public class MainActivity extends AppCompatActivity  {
         eventCard = (CardView) findViewById(R.id.card_view);
         mTextMessage = (TextView) eventCard.findViewById(R.id.info_text);
 
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.drawable.ic_dashboard_black_24dp);
+
+
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbarbutton, menu);
+        return true;
+    }
+
+
 
 }
