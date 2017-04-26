fc9503633f936c6fa121b795d4e51ca6b24ef008
Ushan Abeysekera <ushan95@vt.edu>
2017-04-26 01:28:14 -0400

    Navigation View implemented, fragments implemented and transition

--- app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
+++ app/src/main/java/com/tusk/baton/finalproject/MainActivity.java
@@ -1,45 +1,95 @@
 package com.tusk.baton.finalproject;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
 
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements MyRelaysFragment.OnFragmentInteractionListener, TrendingRelaysFragment.OnFragmentInteractionListener, TrendingLocationsFragment.OnFragmentInteractionListener, SponsoredFragment.OnFragmentInteractionListener, SearchFragment.OnFragmentInteractionListener {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    HashMap<String, Fragment> fragmentHashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        MyRelaysFragment frag1 = new MyRelaysFragment();
+        TrendingRelaysFragment frag2 = new TrendingRelaysFragment();
+        TrendingLocationsFragment frag3 = new TrendingLocationsFragment();
+        SponsoredFragment frag4 = new SponsoredFragment();
+        SearchFragment frag5 = new SearchFragment();
+
+        fragmentHashMap = new HashMap<>();
+        fragmentHashMap.put(getResourceString(R.string.my_relays), frag1);
+        fragmentHashMap.put(getResourceString(R.string.trending_relays), frag2);
+        fragmentHashMap.put(getResourceString(R.string.trending_locations), frag3);
+        fragmentHashMap.put(getResourceString(R.string.sponsored), frag4);
+        fragmentHashMap.put(getResourceString(R.string.search), frag5);
+        setFragment(frag1);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment currentFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_my_relays:
+                    currentFragment = fragmentHashMap.get(getResourceString(R.string.my_relays));
+                    break;
+                case R.id.navigation_trending_relays:
+                    currentFragment = fragmentHashMap.get(getResourceString(R.string.trending_relays));
+                    break;
+                case R.id.navigation_trending_locations:
+                    currentFragment = fragmentHashMap.get(getResourceString(R.string.trending_locations));
+                    break;
+                case R.id.navigation_sponsored:
+                    currentFragment = fragmentHashMap.get(getResourceString(R.string.sponsored));
+                    break;
+                case R.id.navigation_search:
+                    currentFragment = fragmentHashMap.get(getResourceString(R.string.search));
+                    break;
+            }
+
+            if (currentFragment != null) {
+                setFragment(currentFragment);
+                return true;
+            }
+            return false;
+        }
+
+    };
+
+    private void setFragment(Fragment inFrag) {
+        FragmentManager fm = getSupportFragmentManager();
+        for(int i = 0; i < fm.getBackStackEntryCount(); ++i) {
+            fm.popBackStack();
+        }
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.content, inFrag, inFrag.getTag());
+        ft.commit();
+    }
+
+    public String getResourceString(int inRID) {
+        return getResources().getString(inRID);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
